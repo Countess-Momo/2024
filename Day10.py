caf464f2f1e95de44b61c75172f185acbe1bcb44
Countess-Momo <nataliekoponen@gmail.com>
2024-12-18 10:53:04 -0500

    Testing of part 1

---
+++ Day10.py
@@ -0,0 +1,28 @@
+import numpy as np
+import re
+
+def Part1(Input):
+    print(np.where(Input == 0))
+    return
+
+def Part2(Input):
+    return
+
+def InputInitilization(Input):
+    Lines = len(re.findall(r'\n', Input))+1
+    Input = re.sub('\n','',Input)
+    Input = np.array(list(Input)).reshape(Lines,Lines)
+    return Input
+
+
+def main():
+
+    with open("C:\\Users\\natal\\OneDrive\\Documents\\Code\\Learning\\Advent of Codes\\2024\\Input09.txt","r") as x:
+        Input = x.read()
+    
+    Input = InputInitilization(Input)
+    print(Input)
+    Part1(Input)
+
+
+main()
