640d6ff8e7d495deb504e2620ecd1450ff657e6f
Countess-Momo <nataliekoponen@gmail.com>
2024-12-18 11:26:13 -0500

    2

--- Day10.py
+++ Day10.py
@@ -11,18 +11,20 @@ def Part2(Input):
 def InputInitilization(Input):
     Lines = len(re.findall(r'\n', Input))+1
     Input = re.sub('\n','',Input)
+    print(Lines)
     Input = np.array(list(Input)).reshape(Lines,Lines)
     return Input
 
 
 def main():
 
-    with open("C:\\Users\\natal\\OneDrive\\Documents\\Code\\Learning\\Advent of Codes\\2024\\Input09.txt","r") as x:
+    with open("C:\\Users\\natal\\OneDrive\\Documents\\Code\\Learning\\Advent of Codes\\2024\\Input10.txt","r") as x:
         Input = x.read()
     
     Input = InputInitilization(Input)
     print(Input)
     Part1(Input)
+    return
 
 
 main()
